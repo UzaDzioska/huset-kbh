2ffbe745e6b0c4de4063fc56089ffa3951635637
zdzialowska <z.dzialowska@gmail.com>
2018-04-26 13:04:29 +0200

    scrolling fixed, acf fixed

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ let eventlist = document.querySelector("#eventlist");
 let page = 1;
 let lookingForData = false;
 
+//window.addEventListener("scroll", bottomVisible);
+
 function fetchEvents() {
     lookingForData = true;
-    
-    fetch("http://zuzannadzialowska.com/wordpress/wp-json/wp/v2/events?_embed&per_page=5&categories=7,8,9,10&order=asc&page=" + page)
+
+    fetch("http://zuzannadzialowska.com/wordpress/wp-json/wp/v2/events?_embed&per_page=2&categories=7,8,9,10&order=asc&page=" + page)
         .then(e => e.json())
         .then(showEvents)
 }
 
 function showEvents(data) {
-    console.log(data);
+    //console.log(data);
     lookingForData = false;
-    data.forEach(showSingleEvent); 
+    data.forEach(showSingleEvent);
 }
 
 function showSingleEvent(anEvent) {
@@ -22,10 +24,10 @@ function showSingleEvent(anEvent) {
 
     clone.querySelector("h1").textContent = anEvent.title.rendered;
     clone.querySelector(".descript").innerHTML = anEvent.content.rendered;
-    clone.querySelector(".price span").textContent = anEvent.acf.price;
-    /*
- clone.querySelector(".location").textContent = anEvent.acf.location;
- clone.querySelector(".time").textContent = anEvent.acf.time; clone.querySelector(".date").textContent = anEvent.acf.date;*/ //not working
+    clone.querySelector(".event_price span").textContent = anEvent.acf.event_price;
+    clone.querySelector(".event_location").textContent = anEvent.acf.event_location;
+    clone.querySelector(".event_time").textContent = anEvent.acf.event_time;
+    clone.querySelector(".event_date").textContent = anEvent.acf.event_date;
 
     if (anEvent._embedded["wp:featuredmedia"]) { //img is there
         clone.querySelector("img").setAttribute("src", anEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
@@ -33,28 +35,31 @@ function showSingleEvent(anEvent) {
     } else { //no img
         clone.querySelector("img").remove();
     }
-    
-    clone.querySelector('.readmore').href="subpage.html?id=" + anEvent.id;
 
+    clone.querySelector('.readmore').href = "subpage.html?id=" + anEvent.id;
 
     eventlist.appendChild(clone);
 }
 
 fetchEvents();
 
-setInterval(function () {
 
-    if (bottomVisible() && lookingForData === false) {
-        console.log("We've reached rock bottom, fetching articles")
-        page++;
-        fetchEvents();
-    }
-}, 1000)
+//found this stuff online
+setInterval(function(){
+
+  if(bottomVisible() && lookingForData===false){
+    console.log("We've reached rock bottom, fetching articles")
+    page++;
+    fetchEvents();
+  }
+}, 1000);
 
 function bottomVisible() {
-    const scrollY = window.scrollY
-    const visible = document.documentElement.clientHeight
-    const pageHeight = document.documentElement.scrollHeight
-    const bottomOfPage = visible + scrollY >= pageHeight
-    return bottomOfPage || pageHeight < visible
+    console.log("in");
+  const scrollY = window.scrollY
+  const visible = document.documentElement.clientHeight
+  const pageHeight = document.documentElement.scrollHeight-20
+  const bottomOfPage = visible + scrollY >= pageHeight
+  console.log(scrollY+visible, pageHeight, bottomOfPage)
+  return bottomOfPage || pageHeight < visible
 }
